30142b71758429cf0ab0e5ef87b4f7f10ab238a2
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2013-05-01 21:17:26 +0200

    Fixed commit script (again)

--- commit.sh
+++ commit.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 ebook-convert ./Test-Driven\ Development\ -\ Extensive\ Tutorial.epub .mobi
-unzip ./Test-Driven\ Development\ -\ Extensive\ Tutorial.epub
+unzip "Test-Driven Development - Extensive Tutorial.epub"
+
 git add *
 git commit -a -m "$1"
 git push -u origin master
