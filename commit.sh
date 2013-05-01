bd5751741449d07898a5281789ea658d3e2d0c2c
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2013-05-01 21:01:50 +0200

    Fixed commit script to cater for new ebook title

--- commit.sh
+++ commit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ebook-convert ./TDD.epub ./TDD.mobi
-unzip TDD.epub
+ebook-convert ./Test-Driven\ Development\ -\ Extensive\ Tutorial.epub .mobi
+unzip ./Test-Driven\ Development\ -\ Extensive\ Tutorial.epub
 git add *
 git commit -a -m "$1"
 git push -u origin master
