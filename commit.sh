ff27611034d72c113fb23153d5867b36cd5792a0
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2013-05-05 23:08:03 +0200

    Started reusing post on Test-First in a chapter dedicated to this approach. Much refactoring needed for the old post to fit in the tutorial terminology and spirit

--- commit.sh
+++ commit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ebook-convert ./Test-Driven\ Development\ -\ Extensive\ Tutorial.epub .mobi
-cp ./Test-Driven\ Development\ -\ Extensive\ Tutorial.mobi /media/astral/Kindle/documents/
+cp ./Test-Driven\ Development\ -\ Extensive\ Tutorial.mobi /media/astral/Kindle/documents/Test-Driven\ Development\ -\ Extensive\ Tutorial.mobi
 unzip "Test-Driven Development - Extensive Tutorial.epub"
 git add *
 git commit -a -m "$1"
