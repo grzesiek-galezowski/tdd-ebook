9e0d4dd09dabd2d6fd52493b9a2600fee1e4dda9
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2013-05-05 19:50:01 +0200

    Updated commit script to automatically copy to Kindle on my machine

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ebook-convert ./Test-Driven\ Development\ -\ Extensive\ Tutorial.epub .mobi
+cp ./Test-Driven\ Development\ -\ Extensive\ Tutorial.mobi /media/astral/Kindle/documents/
 unzip "Test-Driven Development - Extensive Tutorial.epub"
-
 git add *
 git commit -a -m "$1"
 git push -u origin master
