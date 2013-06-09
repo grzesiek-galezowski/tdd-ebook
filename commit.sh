0b4361de281f5708760d24a5706f5aed425b1c42
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2013-06-09 21:08:19 +0200

    ignoring mobi and pdf output. They will be downloadable from Ubuntu One, so no point in keeping them inside repository

--- commit.sh
+++ commit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ebook-convert ./Test-Driven\ Development\ -\ Extensive\ Tutorial.epub .mobi
+ebook-convert ./Test-Driven\ Development\ -\ Extensive\ Tutorial.epub .pdf
 cp ./Test-Driven\ Development\ -\ Extensive\ Tutorial.mobi /media/astral/Kindle/documents/Test-Driven\ Development\ -\ Extensive\ Tutorial.mobi
 unzip "Test-Driven Development - Extensive Tutorial.epub"
 git add *
