0d68350e520aedb9b24c010e1dbd0ca13d3ef6b3
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2013-10-16 23:18:22 +0200

    Started some work on scope of Statement chapter

--- commit.sh
+++ commit.sh
@@ -5,6 +5,7 @@ ebook-convert ./Test-Driven\ Development\ -\ Extensive\ Tutorial.epub .pdf
 cp ./Test-Driven\ Development\ -\ Extensive\ Tutorial.mobi /media/astral/Kindle/documents/Test-Driven\ Development\ -\ Extensive\ Tutorial.mobi
 unzip "Test-Driven Development - Extensive Tutorial.epub"
 git add *
+git add ./OEBPSOD
 git commit -a -m "$1"
 git push -u origin master
 rm -r ./OEBPS
