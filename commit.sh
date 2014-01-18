04e3d6950e3584c578c33765fffc0d271b1347c6
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2014-01-19 00:23:21 +0100

    a

--- commit.sh
+++ commit.sh
@@ -3,7 +3,7 @@
 ebook-convert ./Test-Driven\ Development\ -\ Extensive\ Tutorial.epub .mobi
 ebook-convert ./Test-Driven\ Development\ -\ Extensive\ Tutorial.epub .pdf --pdf-add-toc --toc-title "Table of Contents" --margin-bottom 20 --margin-top 20 --margin-left 20 --margin-right 20 --pdf-add-toc --change-justification justify
 ebook-convert ./Test-Driven\ Development\ -\ Extensive\ Tutorial.epub .azw3
-cp ./Test-Driven\ Development\ -\ Extensive\ Tutorial.mobi /media/astral/Kindle/documents/Test-Driven\ Development\ -\ Extensive\ Tutorial.mobi
+#cp ./Test-Driven\ Development\ -\ Extensive\ Tutorial.mobi /media/astral/Kindle/documents/Test-Driven\ Development\ -\ Extensive\ Tutorial.mobi
 
 cp ./Test-Driven\ Development\ -\ Extensive\ Tutorial.epub /home/astral/Dropbox/Public/
 cp ./Test-Driven\ Development\ -\ Extensive\ Tutorial.mobi /home/astral/Dropbox/Public/
