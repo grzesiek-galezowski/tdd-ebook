0fe55a11d0ec158cb122dbf7909ea86c5f636e17
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2014-07-30 06:46:26 +0200

    changed title page file name to render better cover on leanpub

--- environment_setup.sh
+++ environment_setup.sh
@@ -7,7 +7,8 @@ sudo apt-get install \
   xdot \
   git \
   default-jdk \
-  inkscape
+  inkscape \
+  ruby-kramdown
 
 
 sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
