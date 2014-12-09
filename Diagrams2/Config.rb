6998417a8b223f04a792e761fe74ab009ebc9d1f
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2014-12-09 20:07:04 +0100

    removed unnecessary directory

--- Diagrams2/Config.rb
+++
@@ -1,9 +0,0 @@
-$DEFAULT_FONT_SIZE=11
-$SMALL_FONT_SIZE=10
-
-def apply_config_to(graph)
-  graph[:fontname] = 'Ubuntu'
-  graph[:fontsize] = $DEFAULT_FONT_SIZE
-  graph[:overlap] = false
-  graph[:splines] = true
-end
